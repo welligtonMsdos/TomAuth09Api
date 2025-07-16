b2769ef939d7a721c953410108ea50851a1d974b
Dell <Dell@DESKTOP-TK7GSO1>
2025-07-16 17:55:45 -0300

    feat: docker-compose

--- Program.cs
+++ Program.cs
@@ -23,6 +23,11 @@ namespace TomAuthApi
 
             builder.Services.AddInterfaces(builder.Configuration);
 
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.ListenAnyIP(80);
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
